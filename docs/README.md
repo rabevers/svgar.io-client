871dcceb253332921263e2f5f13bc9503c8d7636
Richard Bevers <r.a.bevers@saxion.nl>
2022-02-04 23:04:01 +0100

    Added a game loop for various things. Moved movement code from player object to Game object Created a state object for player information

--- docs/README.md
+++ docs/README.md
@@ -5,6 +5,6 @@ The initial Game, Map and Player components have been created. There is no inter
 Rudimentary code was added to have the player follow the mouse. A very important discovery was made... Once the mouse stops moving the player also stops moving. Initially that seemed the idea. However the player will also stop moving if the mouse cursor is nowhere near the player.
 So in order to keep moving the player object the mouse also has to move.
 
-This means we need a different way of handling movement. Thinking of all the things we want to add in later stages it comes to mind we actually need something of a [https://developer.mozilla.org/en-US/docs/Games/Anatomy](game loop).
+This means we need a different way of handling movement. Thinking of all the things we want to add in later stages it comes to mind we actually need something of a [game loop](https://developer.mozilla.org/en-US/docs/Games/Anatomy).
 
 So I will be adding that next.
--- src/components/Game.svelte
+++ src/components/Game.svelte
@@ -4,20 +4,96 @@
      *
      * It will function as the viewport for the map.
      */
+    import { onMount } from 'svelte';
+    import { player } from '../stores/game.js';
+
     import Map from './Map.svelte';
     import Player from './Player.svelte';
 
     export let width: number = 800;     // Create a default width for the map
     export let height: number = 600;    // Create a default height for the map
+
+    let stopLoop;
+    let frameCounter;
+
+    let destination = {
+        x: 0,
+        y: 0,
+    };
+
+    const handleMouseMovement = event => {
+        destination.x = event.clientX;
+        destination.y = event.clientY;
+    };
+
+    const update = (tFrame, steps) => {
+        frameCounter = tFrame;
+        steps.forEach(step => {
+            if (typeof step === 'function') {
+                step();
+            }
+        });
+    };
+
+    onMount(() => {
+        /**
+         * Create and start a game loop
+         * https://developer.mozilla.org/en-US/docs/Games/Anatomy
+         */
+        (function(){
+            function mainLoop(tFrame) {
+                stopLoop = window.requestAnimationFrame( mainLoop );
+                update(tFrame, steps);
+                // render();
+            };
+
+            mainLoop(0);
+        })();
+    });
+
+    // @todo change this to a dynamic value based on mass
+    let speed = 0.5;
+
+    const handleMovement = () => {
+        if ($player.x === destination.x && $player.y === destination.y) {
+            return;
+        }
+        console.log("should move", event);
+        if (destination.x > $player.x) {
+            // player wants to go to the right.
+            // Increase x by speed
+            $player.x += speed;
+        } else if (destination.x < $player.x) {
+            // player wants to go to the right.
+            // Increase x by speed
+            $player.x -= speed;
+        }
+
+        if (destination.y > $player.y) {
+            // player wants to go down.
+            // Increase y by speed
+            $player.y += speed;
+        } else if (destination.y < $player.y) {
+            // player wants to go up.
+            // Dencrease y by speed
+            $player.y -= speed;
+        }
+    }
+
+    let steps = [
+        handleMovement,
+    ];
+
 </script>
 
+<svelte:window on:mousemove={handleMouseMovement}/>
 <svg
     id="game"
     width={width}
     height={height}
 >
     <Map>
-        <Player />
+        <Player playerX={$player.x} playerY={$player.y} />
     </Map>
 </svg>
 
